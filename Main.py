fa52db910ecb3a5af83c4f4fb5277865b17cbea2
andernh <andernh@stud.ntnu.no>
2020-03-06 23:49:07 +0100

    Added manual predicting

--- Main.py
+++ Main.py
@@ -1,3 +1,5 @@
+import math
+
 import pandas
 
 from FetchData import FetchData
@@ -5,14 +7,21 @@ from FetchData import FetchData
 
 def main():
     fetch_data = FetchData()
-    Running_plan = fetch_data.get_weather_forecast()
+    Running_plan = fetch_data.get_data()
     pandas.set_option('display.max_columns', None)
-    print(Running_plan.head(10))
+    print(Running_plan.head(25))
+    sum = 0;
+    for i in range(Running_plan.shape[0]):
+        print(i)
+        number = Running_plan.iloc[i, 2]
+        if not math.isnan(number):
+            roundednumber = int(round(number))
+            print(roundednumber)
+            sum += roundednumber
+    print(sum)
 
-
-
-
-
+    datelist = []
+    for i in range(Running_plan.shape[0]):
 
 
 main()
