c3348f58b82300924029e1b4d99d58fd3c794078
andernh <andernh@stud.ntnu.no>
2020-03-06 22:14:16 +0100

    Updated main
    Made data appear in a readable way

--- Main.py
+++ Main.py
@@ -1,10 +1,13 @@
+import pandas
+
 from FetchData import FetchData
 
 
 def main():
     fetch_data = FetchData()
     Running_plan = fetch_data.get_weather_forecast()
-    print(Running_plan.head(100))
+    pandas.set_option('display.max_columns', None)
+    print(Running_plan.head(10))
 
 
 
