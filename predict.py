a919d1755e20c95713adb446f4467a3f7541a678
andernh <andernh@stud.ntnu.no>
2020-03-07 12:21:40 +0100

    Update predict.py

--- predict.py
+++ predict.py
@@ -6,7 +6,6 @@ import pandas
 from utils import *
 from tqdm import tqdm
 
-
 turbinelist = []
 weatherForecast = []
 runningPlan = []
@@ -15,19 +14,21 @@ net = get_trained_net()
 fetchData = FetchData()
 
 
-def getData(time, t):
+def getWeatherData(time):
     global weatherForecast
+    for i, row in weatherForecast.iterrows():
+        if weatherForecast.iloc[i, 0] == "WP" and weatherForecast.iloc[i, 1] == time:
+            return [turbine_to_number("T01"), weatherForecast.iloc[i, :]['SUB_WIND_SPEED_110'],
+                    weatherForecast[i, :]["SUB_WIND_DIR_110"],4000, weatherForecast[i, :]["SUB_AIR_TEMP"],1]
+def getRunningData(t):
     global runningPlan
     pwlim = 0
     state = 0
     for i, row in runningPlan.iterrows():
-        if runningPlan.iloc[i,0] == t:
-            pwlim = runningPlan.iloc[i,2]
+        if runningPlan.iloc[i, 0] == t:
+            pwlim = runningPlan.iloc[i, 2]
             state = runningPlan.iloc[i, 3]
-    for i, row in weatherForecast.iterrows():
-        if weatherForecast.iloc[i,0] == "WP" and weatherForecast.iloc[i,1] == time:
-            return [turbine_to_number(t),weatherForecast.iloc[i,:]['SUB_WIND_SPEED_110'],weatherForecast[i,:]["SUB_WIND_DIR_110"],pwlim,weatherForecast[i,:]["SUB_AIR_TEMP"],state]
-
+    return pwlim, state
 
 def predict_48(from_time):
     global weatherForecast
@@ -53,17 +54,14 @@ def predict_48(from_time):
 
 def predict_hour(time):
     sum = 0
+    datalist = getWeatherData(time)
     for t in turbinelist:
-        sum += predict_turbine(time, t)
+        sum += predict_turbine(time, t, datalist)
     return sum
 
 
-def predict_turbine(time, turbine):
-    data = getData(time, turbine)
+def predict_turbine(time, turbine, data):
     return net(torch.tensor(data)).item()
-
-
-
 
 
 def get48hourdatetimelist(start_time):
@@ -88,4 +86,3 @@ def get48hourdatetimelist(start_time):
                 numsplit[1] = numsplit[1] + 1
         datelist.append(str(numsplit[0]) + "." + str(numsplit[1]) + "." + str(numsplit[2]) + " " + str(hour) + ":00")
     return datelist
-
