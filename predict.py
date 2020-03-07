f23b41625b1ffaf12b39ff536a10918a3b70e3fc
Tor <tor-ds@hotmail.com>
2020-03-07 13:24:08 +0100

    48 hours spagettikode
    takk, Anders

--- predict.py
+++ predict.py
@@ -39,6 +39,39 @@ class Predictor():
             turbine_data = self.running_plan[self.running_plan['turbine'] == turbine][['ActivePowerLimit', 'StateRun']]
             turbine_data = turbine_data[turbine_data['timestamp'] == timestamp]
 
+    def get48hourdatetimelist(start_time):
+        datelist = []
+        start_time = start_time.replace(" ", ".")
+        split = start_time.split(".")
+        numsplit = []
+        print(split)
+        hourandminute = split[3].split(":")
+        hour = int(hourandminute[0])
+        for i in range(len(split)):
+            if i != 3:
+                numsplit.append(int(split[i]))
+        year = numsplit[0]
+        numsplit[0] = numsplit[2]
+        numsplit[2] = year
+        for i in range(48):
+            if (hour == 24):
+                hour = 0
+                numsplit[0] = numsplit[0] + 1
+                if numsplit[1] % 2 == 0:
+                    if numsplit[0] > 30:
+                        numsplit[0] = 0
+                        numsplit[1] = numsplit[1] + 1
+                else:
+                    if numsplit[0] > 31:
+                        numsplit[0] = 0
+                        numsplit[1] = numsplit[1] + 1
+            day = numsplit[0]
+            numsplit[0] = numsplit[2]
+            numsplit[2] = day
+            datelist.append(
+                str(numsplit[0]) + "." + str(numsplit[1]) + "." + str(numsplit[2]) + " " + str(hour) + ":00")
+            hour = hour + 1
+            return datelist
             
             
 
