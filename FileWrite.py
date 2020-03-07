088c1f65fc43a23328699d13c77c0fec3a961f99
Øyvind Monsen <yvind.monsen@gmail.com>
2020-03-07 13:43:15 +0100

    think it works now

--- FileWrite.py
+++ FileWrite.py
@@ -1,10 +1,12 @@
 import pandas as pd
-from predict import predict_48
+from predict import Predictor
 
 def listTo(predList):
     df = pd.DataFrame(predList, columns=["Timestamp", "Energy"])
     df.to_csv('data/testList.csv', index=False, header=True)
 
 if __name__ == "__main__":
-    listTo(predict_48("2018.12.17 00:00+00:00"))
+    predictor = Predictor()
+    predictor.predict_48('06.01.2019 00:00')
+
 
