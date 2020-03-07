c1dab55ed77b65468452f739243c49fe1e7c0e10
andernh <andernh@stud.ntnu.no>
2020-03-07 08:30:41 +0100

    Changed estimate to own method

--- Main.py
+++ Main.py
@@ -5,25 +5,30 @@ import pandas
 from FetchData import FetchData
 
 
-def main():
-    fetch_data = FetchData()
-    Running_plan = fetch_data.get_data()
-    pandas.set_option('display.max_columns', None)
-    print(Running_plan.head(0))
+def educatedguess(Running_plan):
     sum = 0;
     for i in range(Running_plan.shape[0]):
         number = Running_plan.iloc[i, 2]
         if not math.isnan(number):
             roundednumber = int(round(number))
             sum += roundednumber
-    print(sum)
+    print("Sum of all power generated for all hour for all turbines is: " + sum)
 
     datelist = []
     for i in range(Running_plan.shape[0]):
         s = Running_plan.iloc[i, 1]
         if datelist.count(s) == 0:
             datelist.append(s)
-    print(sum/len(datelist))
+    print("Sum of power generated for all turbines each hour: " + str(sum / len(datelist)))
+
+    turbinelist[]
+
+
+def main():
+    fetch_data = FetchData()
+    Running_plan = fetch_data.get_data()
+    pandas.set_option('display.max_columns', None)
+    print(Running_plan.head(0))
 
 
 main()
