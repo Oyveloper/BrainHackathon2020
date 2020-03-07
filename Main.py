c65e5609329c2e8577952283b5520de3c4c1659d
andernh <andernh@stud.ntnu.no>
2020-03-07 08:41:59 +0100

    Made estimate function

--- Main.py
+++ Main.py
@@ -12,23 +12,30 @@ def educatedguess(Running_plan):
         if not math.isnan(number):
             roundednumber = int(round(number))
             sum += roundednumber
-    print("Sum of all power generated for all hour for all turbines is: " + sum)
+    print("Sum of all power generated for all hour for all turbines is: " + str(sum))
 
     datelist = []
     for i in range(Running_plan.shape[0]):
         s = Running_plan.iloc[i, 1]
         if datelist.count(s) == 0:
             datelist.append(s)
-    print("Sum of power generated for all turbines each hour: " + str(sum / len(datelist)))
+    eachHourProduce = sum / len(datelist)
+    print("Sum of power generated for all turbines each hour: " + str(eachHourProduce))
 
-    turbinelist[]
-
+    turbinelist = []
+    for i in range(Running_plan.shape[0]):
+        turb = Running_plan.iloc[i, 0]
+        if turbinelist.count(turb) == 0:
+            turbinelist.append(turb)
+    eachHourEachTurbine = eachHourProduce / len(turbinelist)
+    print("Average power each hour each turbine: " + str(eachHourEachTurbine))
 
 def main():
     fetch_data = FetchData()
     Running_plan = fetch_data.get_data()
     pandas.set_option('display.max_columns', None)
     print(Running_plan.head(0))
+    educatedguess(Running_plan)
 
 
 main()
