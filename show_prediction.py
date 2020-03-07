8cb557b940f935f666259b230506da9dc222675b
Øyvind Monsen <yvind.monsen@gmail.com>
2020-03-07 19:26:15 +0100

    plotting prediction

---
+++ show_prediction.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+data = pd.read_csv("data/submission_5_lorien.csv", sep=";").sort_values(by='timestamp', ascending=False)
+
+data.plot(x='timestamp', y='energy', kind='line')
+plt.show()
